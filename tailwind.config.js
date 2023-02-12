ee88592208a7166ef9ed49aa5ad7cb03e8bc6c51
PHOENIX00710 <103223159+PHOENIX00710@users.noreply.github.com>
2023-02-12 15:06:42 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./index.html"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    extend: {
-      
-    },
+    extend: {},
   },
   plugins: [],
 }
